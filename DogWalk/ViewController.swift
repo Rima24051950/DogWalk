de9ee72452751d3a54a37e25dd6d58b47b28f178
Rima <rimarz@yandex.ru>
2026-01-30 12:23:04 +0300

    Initial Commit

---
+++ DogWalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogWalk
+//
+//  Created by MacBook on 30.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
